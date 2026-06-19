566df3480523a0fbf58e7efe60ad81f4f75d86cd
Taha Kurhan <kurhantaha07@hotmail.com>
2026-06-19 18:11:28 +0300

    Commit Update -Final arrangements

--- hotelcss-frontend/src/main.jsx
+++ hotelcss-frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'material-symbols/outlined.css'  // self-hosted icon font (works offline / behind captive portal)
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
